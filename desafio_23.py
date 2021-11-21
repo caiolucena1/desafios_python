cc3c64aaafaa9631f9e8fa3790af5e4255d827a3
Caio Lucena <caio.lucena1@gmail.com>
2021-11-21 18:14:11 -0300

    Desafios 23 e 25

--- desafio_23.py
+++ desafio_23.py
@@ -5,8 +5,14 @@ payment = float(input())
 if payment <= 2000.00:
     print('Isento')
 elif 2000 < payment <= 3000: # payment > 2000 and payment <= 3000
-    diff = payment - 2000
-    tax = diff * 0.08
-    print(f'R$ {tax:.2f}')
-
-'''T R A V E I !'''
+    tax = (payment - 2000) * 0.08
+    print(f'R$ {tax:.2f}') 
+elif 3000 < payment <= 4500:
+    a = 1000 * 0.08
+    b = (payment - 3000) * 0.18
+    print(f'R$ {a + b:.2f}')
+elif payment > 4500:
+    a = 1000 * 0.08
+    b = 1500 * 0.18
+    c = (payment - 4500) * 0.28
+    print(f'R$ {a + b + c:.2f}')
---
+++ desafio_25.py
@@ -0,0 +1,9 @@
+#URI 1060
+
+x = 0
+
+for i in range(6):
+    a = float(input())
+    if a > 0:
+        x = x + 1
+print(f'{x} valores positivos')
