358eab46b1dc98b66e68331c8f5967585b095b10
Caio Lucena <caio.lucena1@gmail.com>
2021-11-22 20:19:52 -0300

    desafio_19

--- desafio_19.py
+++ desafio_19.py
@@ -1,20 +1,40 @@
 #URI 1047
 
-time = input().split()
-start_hour = int(time[0])
-start_minute = int(time[1])
-end_hour = int(time[2])
-end_minute = int(time[3])
+entrada = input().split()
+h_start = int(entrada[0])
+m_start = int(entrada[1])
+h_end = int(entrada[2])
+m_end = int(entrada[3])
+
+total_minutes_start = h_start*60 + m_start #transformando horas em minutos e somando aos minutos ja existentes
+total_minutes_end = h_end*60 + m_end #vale o comentário de cima
+
+if total_minutes_start < total_minutes_end:
+    dur = total_minutes_end - total_minutes_start
+    hours = dur//60 #transformando os minutos em hora
+    min = dur % 60 #pegando os minutos do resto da divisão
+
+elif total_minutes_start >= total_minutes_end:
+    dur = (1440 - total_minutes_start) + total_minutes_end #1440 minutos equivalem a 24h
+    hours = dur//60
+    min = dur % 60
+print(f'O JOGO DUROU {hours} HORA(S) E {min} MINUTO(S)')
 
 
+#Nere, eu não me dei bem com datetime
 
+'''
+import datetime
 
-# T R A V E I
+entrada = input().split()
+h_start = int(entrada[0])
+m_start = int(entrada[1])
+h_end = int(entrada[2])
+m_end = int(entrada[3])
 
+a = datetime.time(h_start, m_start)
+b = datetime.time(h_end, m_end)
+c= b - a
 
-'''if start_hour < end_hour:
-    total_hours = end_hour - start_hour
-    total_minutes = end_minute - start_minute
-
-print(f'O JOGO DUROU {total_hours} HORA(S) E {total_minutes} MINUTO(S)')'''
-
+print(a, b, sep='\n')
+'''
