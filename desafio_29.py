6bb0d38ee645005cdfc5666b34db6d7d78b819ae
Caio Lucena <caio.lucena1@gmail.com>
2021-12-06 21:07:27 -0300

    desafio_29

--- desafio_29.py
+++ desafio_29.py
@@ -2,10 +2,13 @@
 
 entrada = float(input())
 
+if 0 <= entrada >= 10:
+    entrada_2 = float(input())
+
 while entrada < 0 or entrada > 10:
     print('nota invalida')
     entrada = float(input())
-    if 0 < entrada <= 10:
+    if 0 <= entrada <= 10:
         entrada_2 = float(input())
 while entrada_2 < 0 or entrada_2 > 10:
     print('nota invalida')
@@ -13,6 +16,7 @@ while entrada_2 < 0 or entrada_2 > 10:
 if 0 < entrada_2 <= 10:
     media = (entrada + entrada_2)/2
 print(f'media = {media:.2f}')
+
 
 '''
 cont=0 
@@ -32,8 +36,8 @@ while cont<2:
         soma+=n2
         cont+=1
 
-    if(n1>=0 and n1<=10 and n1>=0 and n2<=10):
+    if(n1>=0 and n1<=10 and n2>=0 and n2<=10):
         cont+=2  
         break
-    print("media = %.2f" %(soma/2))
+print(f"media = {soma/2:.2f}")
     '''
